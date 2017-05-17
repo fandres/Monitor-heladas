50ff16eed52508f691299792b99e28e615732c96
fandres <fabian.salamanca@openmailbox.org>
2017-05-17 13:58:24 -0500

    Suspension and reactivation of the alarm Reorder code

--- Broker/python_paho_broker.py
+++ Broker/python_paho_broker.py
@@ -1,3 +1,20 @@
+"""
+    DESCRIPCI?N...
+    This software is free, licensed y distributed under GPL v3.
+    (see COPYING) WITHOUT ANY WARRANTY.
+    You can see a description of license here: http://www.gnu.org/copyleft/gpl.html
+    Copyright(c) 2017 by fandres "Fabian Salamanca" <fabian.salamanca@openmailbox.org>
+                      by CH3 "Christian Camilo Cardenas <christian.camilo.cardenas@gmail.com>"
+    Distributed under GPLv3+
+    Hardware: ESP8266
+    Pin distribution(Default)
+    Pin 17 [GPIO.BCM]   ->  Pin: Alarm(Buzer): [OUT]
+    Pin 4 [GPIO.BCM]    ->  Pin: Stop Alarm(Buzer): [OUT]
+    Configure wifi and MQTT
+    library:
+    https://github.com/eclipse/paho.mqtt.python
+"""
+
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
 import paho.mqtt.publish as publish
@@ -5,12 +22,14 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-HOSTNAME = "169.254.176.163"
+HOSTNAME = "169.254.176.163"  # <---------------------- CONFIGURE BROKER ADDRESS
 PIN_ALARMA = 17
 PIN_STOP_ALARMA = 4
 SET_TEMP = 30
 SET_HUM = 55
+TIME_SLEEP_ALARMA = 10
 alarm_state = 0
+count_alarma = 0
 
 
 def on_connect(mqttc, obj, flags, rc):
@@ -35,19 +54,31 @@ print("%s %s" % (msg_temp.topic, msg_temp.payload))
 print("%s %s" % (msg_hum.topic, msg_hum.payload))
 
 
-while True:
-    if float(msg_temp.payload) <= SET_TEMP and float(msg_hum.payload) > SET_HUM:
-        print(" Helada! Ring Ring")
-        if GPIO.input(PIN_STOP_ALARMA) == 1:
-            alarm_state = 1
-            publish.single("/cultivo/alarma", "Alarma Apagada", hostname=HOSTNAME)
-        sleep(0.5)
-        if alarm_state == 0:
-            publish.single("/cultivo/alarma", "on", hostname=HOSTNAME)
-            GPIO.output(PIN_ALARMA, True)
-        else:
-            GPIO.output(PIN_ALARMA, False)
-            publish.single("/cultivo/alarma", "off", hostname=HOSTNAME)
-        #sleep(1)
-else:
+try:
+    while True:
+        #print("%s %s" % (msg_temp.topic, msg_temp.payload))
+        #print("%s %s" % (msg_hum.topic, msg_hum.payload))
+        "Estado de Alarma"
+        if float(msg_temp.payload) <= SET_TEMP and float(msg_hum.payload) > SET_HUM:
+            print(" Helada! Ring Ring")
+            if GPIO.input(PIN_STOP_ALARMA) == 1:
+                alarm_state = 1
+                publish.single("/cultivo/alarma", "Alarma Apagada", hostname=HOSTNAME)
+                count_alarma = 1
+                sleep(0.2)
+            if alarm_state == 0:
+                publish.single("/cultivo/alarma", "on", hostname=HOSTNAME)
+                GPIO.output(PIN_ALARMA, True)
+            else:
+                GPIO.output(PIN_ALARMA, False)
+                publish.single("/cultivo/alarma", "off", hostname=HOSTNAME)
+                if count_alarma >= 1 and count_alarma < TIME_SLEEP_ALARMA:
+                    count_alarma += 1
+                    sleep(0.5)
+                elif count_alarma == TIME_SLEEP_ALARMA:
+                    count_alarma = 0
+                    alarm_state = 0
+        elif count_alarma == TIME_SLEEP_ALARMA:
+            count_alarma = 0
+finally:
     GPIO.cleanup()
